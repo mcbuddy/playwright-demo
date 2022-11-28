02aeea48f825566e77a1852fc862c01b7783d1c6
Budi Sugianto <budisugianto777@gmail.com>
2022-11-27 21:38:56 -0500

    use headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,16 @@
 // playwright.config.ts
 import type { PlaywrightTestConfig } from '@playwright/test';
+import { env } from 'process';
+
+var is_headless = false
+if(env.HEADLESS == 'true') {
+    is_headless  = true
+}
 
 const config: PlaywrightTestConfig = { 
     timeout: 60 * 1000, // Setup timeout to 5 minutes. 
     use: {    
-        headless: false, // Turn off headless mode.  
+        headless: is_headless, // Turn off headless mode.  
     },
 };
 
